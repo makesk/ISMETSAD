86761f051831df4cab51a83efc6e7150548a5efd
kirkekr <36279336+kirkekr@users.noreply.github.com>
2020-03-06 20:30:30 +0200

    Test file

---
+++ TestIS.py
@@ -0,0 +1,70 @@
+#T1
+post('test', {'Arenguklass':'Noorendik', 
+              'Peapuuliik' : 'Mänd', 
+              'Vanus':52})  
+
+#T2
+post('test', {'Peapuuliik':'Hall pähklipuu',
+              'Vanus':90,   
+              'Looduskaitsealune':'jah',
+              'Boniteediklass':'5' }) 
+#T3
+post('test', {'Arenguklass':'latimets', 
+              'Peapuuliik' : 'Kask', 
+              'Puudearv':33000})
+#T4
+post('test', {'Arenguklass':'latimets', 
+              'Peapuuliik' : 'Kask', 
+              'Puudearv':1500, 
+              'Vanus':71,  
+              'Diameeter':8,  
+              'Boniteediklass':'3' }) 
+###
+#T5
+post('test', {'Arenguklass':'latimets',  
+              'Peapuuliik' : 'Mänd', 
+              'Vanus':90,   
+              'Diameeter':11,   
+              'Boniteediklass':'2' }) 
+#T6
+post('test', {'Arenguklass':'latimets',  
+              'Peapuuliik' : 'Kask', 
+              'Vanus':35,   
+              'Diameeter':9})  
+#T7
+post('test', {'Arenguklass':'Küpsmets',  
+              'Peapuuliik' : 'Haab', 
+              'Vanus':40,   
+              'Diameeter':15,   
+              'Boniteediklass':'2' }) 
+#T8
+post('test', {'Peapuuliik':'Mänd', 
+              'Vanus':90, 
+              'Boniteediklass':'1A' }) 
+#T9
+post('test', {'Linnurahu':'jah',
+              'Peapuuliik':'Sanglepp',
+             'Boniteediklass':'4'}) 
+#T10
+post('test', {'Peapuuliik' : 'Kask', 
+              'Vanus':80, 
+              'Boniteediklass':'5' })
+#T11
+post('test', {'Kahjustus' : 'Tugev', 
+              'Peapuuliik':'Nulg',
+              'Linnurahu':'Ei' })
+#T12
+post('test', {'Arenguklass':'latimets',  
+              'Peapuuliik' : 'Sanglepp', 
+              'Vanus':80,  
+              'Kasvukoht':'Madalsoo' })
+#T13
+post('test', {'Arenguklass':'latimets',
+              'Peapuuliik' : 'Sanglepp', 
+              'Vanus':80,   
+              'Diameeter':8,   
+              'Kasvukoht':'Osja' })
+#T14
+post('test', {'Peapuuliik' : 'Mänd',
+              'Vanus':40,  
+              'Kasvukoht':'Kõdusoo' })
